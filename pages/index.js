e8f77b95d02f25c4643b6ab6f30251639d24728c
Álvaro Junqueira <46428190+Tserewara@users.noreply.github.com>
2025-03-14 08:11:19 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,26 +11,27 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hey, I'm <strong>Álvaro.</strong></p>
-        <p>
-          I'm a Software Engineer. I like to learn and teach.
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Articles</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
+      <p title="Algo incrível vai acontecer aqui!" style="text-align: center; font-size: 24px;">Marĩ ĩwẽ uptabi te dza watobro ãmemhã!</p>
+      // <section className={utilStyles.headingMd}>
+      //   <p>Hey, I'm <strong>Álvaro.</strong></p>
+      //   <p>
+      //     I'm a Software Engineer. I like to learn and teach.
+      //   </p>
+      // </section>
+      // <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      //   <h2 className={utilStyles.headingLg}>Articles</h2>
+      //   <ul className={utilStyles.list}>
+      //     {allPostsData.map(({ id, date, title }) => (
+      //       <li className={utilStyles.listItem} key={id}>
+      //       <Link href={`/posts/${id}`}>{title}</Link>
+      //       <br />
+      //       <small className={utilStyles.lightText}>
+      //         <Date dateString={date} />
+      //       </small>
+      //     </li>
+      //     ))}
+      //   </ul>
+      // </section>
       
     </Layout>
   );
