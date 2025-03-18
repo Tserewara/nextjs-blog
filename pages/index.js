d24bdf50e396c17661630385e384e820fd0f3706
alvaro.junqueira <alvaro.junqueira@eemovel.com>
2025-03-18 08:58:38 -0300

    fix: broken comment

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
 import { getSortedPostData } from '../lib/posts';
 
 export default function Home({ allPostsData }) {
@@ -11,27 +8,32 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <p title="Algo incrível vai acontecer aqui!" style="text-align: center; font-size: 24px;">Marĩ ĩwẽ uptabi te dza watobro ãmemhã!</p>
-      // <section className={utilStyles.headingMd}>
-      //   <p>Hey, I'm <strong>Álvaro.</strong></p>
-      //   <p>
-      //     I'm a Software Engineer. I like to learn and teach.
-      //   </p>
-      // </section>
-      // <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-      //   <h2 className={utilStyles.headingLg}>Articles</h2>
-      //   <ul className={utilStyles.list}>
-      //     {allPostsData.map(({ id, date, title }) => (
-      //       <li className={utilStyles.listItem} key={id}>
-      //       <Link href={`/posts/${id}`}>{title}</Link>
-      //       <br />
-      //       <small className={utilStyles.lightText}>
-      //         <Date dateString={date} />
-      //       </small>
-      //     </li>
-      //     ))}
-      //   </ul>
-      // </section>
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+      <p title="Algo incrível vai acontecer aqui! Espere um pouco!" style={{textAlign: "center", fontSize: "24px", cursor: "pointer"}}>
+        Marĩ ĩwẽ uptabi te dza watobro ãmemhã! Sapari a'ö!
+      </p>
+      <img src="/images/Designer.jpeg" className="img-profile" alt="Álvaro" style={{height: "300px"}}/>
+      </div>
+      {/* <section className={utilStyles.headingMd}>
+        <p>Hey, I'm <strong>Álvaro.</strong></p>
+        <p>
+          I'm a Software Engineer. I like to learn and teach.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Articles</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section> */}
       
     </Layout>
   );
